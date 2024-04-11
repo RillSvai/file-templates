395ec1000ca408e16e24f858ad066765a3105b48
RillSvai <kirilkvas07@gmail.com>
2024-04-11 18:03:24 +0300

    fix: remove redundant psql checker

--- rust/scripts/init_db/sqlx_postgres.sh
+++ rust/scripts/init_db/sqlx_postgres.sh
@@ -2,11 +2,6 @@
 set -eo pipefail
 set -x
  
-if ! [ -x "$(command -v psql)" ]; then 
-    echo >&2 "Error: psql is not installed."
-    exit 1
-fi 
-
 if ! [ -x "$(command -v sqlx)" ]; then
     echo >&2 "Error: sqlx is not installed."
     echo >&2 "Use:"
